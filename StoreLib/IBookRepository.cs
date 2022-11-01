8263be6435f5c6587843bd4b29a6a837622c0eb9
Bazinga Bazinga <matvei2in@gmail.com>
2022-11-01 12:58:23 +0500

    Создать интерфейс хранилища книг

---
+++ StoreLib/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreLib
+{
+    public interface IBookRepository
+    {
+        Book[] GetAllByTitle(string title);
+    }
+}
