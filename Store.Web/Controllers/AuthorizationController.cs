fa703a5c1d1b8eeb71c0b7891b1d371eb451270b
Bazinga Bazinga <matvei2in@gmail.com>
2023-01-03 20:30:02 +0500

    Добавить контроллеры: регистрации, авторизации.

---
+++ Store.Web/Controllers/AuthorizationController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Web.App.models;
+using Store.Web.App.services;
+
+namespace Store.Web.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        private readonly AuthorizationService _authorizationService;
+        public AuthorizationController(AuthorizationService authorizationService)
+        {
+            _authorizationService = authorizationService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(AuthorizationModel user)
+        {
+            if(ModelState.IsValid)
+            {
+                var isSuccess = await _authorizationService.AuthorizationAsync(user);
+                if (isSuccess)
+                    return Redirect("/Home/Index");
+            }
+            return View(user);
+        }
+    }
+}
