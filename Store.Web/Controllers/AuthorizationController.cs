053fb5ed45a3a79781ed629b78085eedcf26f67d
Bazinga Bazinga <matvei2in@gmail.com>
2023-01-16 21:09:57 +0500

    Настроить сокрытие элементов панели навигации.

--- Store.Web/Controllers/AuthorizationController.cs
+++ Store.Web/Controllers/AuthorizationController.cs
@@ -12,7 +12,6 @@ namespace Store.Web.Controllers
         {
             _authorizationService = authorizationService;
         }
-
         [HttpGet]
         public IActionResult LogIn()
         {
